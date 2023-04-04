42647730544a6d5f6a8a6fce081544148d5bf06e
BRIGHT <87952798+BrightBoakye@users.noreply.github.com>
2023-04-04 16:29:46 +0100

    Update bioGeneration.py

--- back-end-function/bioGeneration.py
+++ back-end-function/bioGeneration.py
@@ -10,12 +10,24 @@ def bioWriting(**serializer_data):
 
     #building prompt for AI
     prompt= 'Write bio with the next information:\n'\
-        'first name: {}\n'\
-        'last name: {}\n'\
-        'date of birth: {}\n'\
-        'phone number: {}\n'\
-        'adress line: {}\n'\
-        'email: {}\n'\
+        'first_name: {}\n'\
+        'last_name: {}\n'\
+        'date_of_birth: {}\n'\
+        'phone_number: {}\n'\
+        'address_line: {}\n'\
+        'tone: {}\n'\
+        'strength: {}\n'\
+        'email_address: {}\n'\
+        'institution: {}\n'\
+        'title: {}\n'\
+        'level_of_education: {}\n'\
+        'graduation_year: {}\n'\
+        'skills: {}\n'\
+        'company_name: {}\n'\
+        'role: {}\n'\
+        'work_duration: {}\n'\
+        'work_achievments: {}\n'\
+        
         'The bio must have a {} tone and emphatize on {}.'.format(
             serializer_data['first_name'],
             serializer_data['last_name'],
@@ -25,6 +37,15 @@ def bioWriting(**serializer_data):
             serializer_data['email_address'],
             serializer_data['tone'],
             serializer_data['strength']
+            serializer_data['institution']
+            serializer_data['title']
+            serializer_data['level_of_education']
+            serializer_data['graduation_year']
+            serializer_data['skills']
+            serializer_data['company_name']
+            serializer_data['role']
+            serializer_data['work_duration']
+            serializer_data['work_achievments']
         )
     try:
         prompt = '\n'.join([prompt,
@@ -51,5 +72,5 @@ bio = bioWriting(first_name = 'Jhon',
                  address_line = '123 Baker St', 
                  email_address = 'jhon@gmail.com', 
                  tone = 'hard', 
-                 strength = 'pacience',)
+                 strength = 'patience',)
 print(bio)
